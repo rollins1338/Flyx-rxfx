b681ba502844d4ec5590aa2ed68efe0a65604791
Vynx-Velvet <nicholasjmagnusson@gmail.com>
2025-12-13 10:09:53 -0600

    fix about page

--- app/(routes)/about/AboutPageClient.tsx
+++ app/(routes)/about/AboutPageClient.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { Navigation } from '@/components/layout/Navigation';
 import { Footer } from '@/components/layout/Footer';
